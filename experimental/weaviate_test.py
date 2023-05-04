49c1847b48767b7e9c85acfec5fe5b1565a8fb08
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2023-05-04 16:35:09 +1200

    Add data import to weaviate_test.py

--- experimental/weaviate_test.py
+++ experimental/weaviate_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import weaviate
 from dotenv import load_dotenv
@@ -20,4 +21,38 @@ client = weaviate.Client(
 #
 # client.schema.create_class(class_obj)
 
-print(client.schema.get())
+# # ===== import data =====
+# # Load data
+# import requests
+# url = 'https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json'
+# resp = requests.get(url)
+# data = json.loads(resp.text)
+#
+# # Configure a batch process
+# with client.batch as batch:
+#     batch.batch_size = 100
+#     # Batch import all Questions
+#     for i, d in enumerate(data):
+#         print(f"importing question: {i + 1}")
+#
+#         properties = {
+#             "answer": d["Answer"],
+#             "question": d["Question"],
+#             "category": d["Category"],
+#         }
+#
+#         client.batch.add_data_object(properties, "Question")
+
+# print(client.query.aggregate('Question').with_meta_count().do())
+
+nearText = {"concepts": ["biology"]}
+
+result = (
+    client.query
+    .get("Question", ["question", "answer", "category"])
+    .with_near_text(nearText)
+    .with_limit(2)
+    .do()
+)
+
+print(json.dumps(result, indent=4))
