4edeef7617edef6557a08a88fc54fcc6f59b6f94
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2023-05-26 16:37:12 +1200

    Add flask_test and template html

---
+++ experimental/flask_test.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+import realm_weaver
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    response = realm_weaver.construct_background_prompt(user_text)
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
